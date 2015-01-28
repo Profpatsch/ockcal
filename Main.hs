25686625a56f5d553984c84edfd13c692f64a9f4
lukasepple <git@lukasepple.de>
2015-01-28 20:31:54 +0100

    basic subcommand parsing

--- Main.hs
+++ Main.hs
@@ -1,7 +1,8 @@
-import Data.Time (Day, TimeOfDay, getCurrentTime)
+import Data.Time (Day, TimeOfDay)
 import Data.List ()
 import Data.Maybe (fromJust)
 import System.Command (runCommand, proc, waitForProcess, isSuccess)
+import System.Environment (getArgs, getProgName)
 
 readCalendarEvent :: String -> ((Day, TimeOfDay), String)
 readCalendarEvent line = ((read $ head $ dateAndTime, read $ (head . tail) $ dateAndTime), eventText)
@@ -11,9 +12,9 @@ readCalendarEvent line = ((read $ head $ dateAndTime, read $ (head . tail) $ dat
 showCalendarEvent :: ((Day, TimeOfDay), String) -> String
 showCalendarEvent ((day, time), string) = "On " ++ show day ++ " at " ++ show time ++ ": " ++ string
 
-main :: IO ()
-main = do
---  now <- getCurrentTime
+listCalendarEntries :: IO ()
+listCalendarEntries = do
+  calendar <- readFile "cal.txt"
   printCalendar <- runCommand "cal"
   exitCode <- waitForProcess printCalendar
   
@@ -21,9 +22,25 @@ main = do
     then putStrLn "Warning: cal seems not to work. Have you installed it correctly?"
     else return ()
 
-  calendar <- readFile "cal.txt"
-
   let events = map readCalendarEvent $ lines $ calendar
       printable = unlines $ map (((++) "- ") . showCalendarEvent) events
 
   putStr printable
+
+addCalendarEntry :: IO ()
+addCalendarEntry = return ()
+
+printUsage :: IO ()
+printUsage = do
+  prog <- getProgName
+  putStrLn $ prog ++ " [list|add]"
+
+main :: IO ()
+main = do
+  args <- getArgs
+
+  case args of
+       []         -> listCalendarEntries
+       ("list":_) -> listCalendarEntries
+       ("add":_)  -> addCalendarEntry
+       _          -> printUsage
