275da651bc9fea95a8d2d32f7f6a057f3d4020cb
lukasepple <git@lukasepple.de>
2015-01-28 21:27:48 +0100

    clean up

--- Main.hs
+++ Main.hs
@@ -1,11 +1,20 @@
 import Data.Time (Day, TimeOfDay)
 import Data.List ()
-import Data.Maybe (fromJust)
-import System.Command (runCommand, proc, waitForProcess, isSuccess)
+import System.Command (runCommand, waitForProcess, isSuccess)
 import System.Environment (getArgs, getProgName)
+import Control.Monad (unless)
+
+calendarFile :: String
+calendarFile = "cal.txt"
+
+-- the file format of the calendarFile is as follows:
+-- YYYY-MM-DD HH:MM:SS | <event title>
+--
+-- it currently gets parsed using read
+-- I will evaluate if there's any problem doing this
 
 readCalendarEvent :: String -> ((Day, TimeOfDay), String)
-readCalendarEvent line = ((read $ head $ dateAndTime, read $ (head . tail) $ dateAndTime), eventText)
+readCalendarEvent line = ((read $ head dateAndTime, read $ (head . tail) dateAndTime), eventText)
   where dateAndTime = words (takeWhile (/= '|') line)
         eventText   = dropWhile (== ' ') $ tail $ dropWhile (/= '|') line
 
@@ -14,21 +23,22 @@ showCalendarEvent ((day, time), string) = "On " ++ show day ++ " at " ++ show ti
 
 listCalendarEntries :: IO ()
 listCalendarEntries = do
-  calendar <- readFile "cal.txt"
+  calendar <- readFile calendarFile
+
+  -- use the coreutil cal to print a nice calendar
   printCalendar <- runCommand "cal"
   exitCode <- waitForProcess printCalendar
   
-  if not $ isSuccess $ exitCode
-    then putStrLn "Warning: cal seems not to work. Have you installed it correctly?"
-    else return ()
+  unless (not $ isSuccess exitCode) (error "Unable to call the coreutil 'cal'")
 
-  let events = map readCalendarEvent $ lines $ calendar
-      printable = unlines $ map (((++) "- ") . showCalendarEvent) events
+  -- generate a simple listning of the events noted in the calendar
+  let events = map readCalendarEvent $ lines calendar
+      printable = unlines $ map (("- " ++) . showCalendarEvent) events
 
   putStr printable
 
-addCalendarEntry :: IO ()
-addCalendarEntry = return ()
+addCalendarEntry :: [String] -> IO ()
+addCalendarEntry _ = return ()
 
 printUsage :: IO ()
 printUsage = do
@@ -42,5 +52,5 @@ main = do
   case args of
        []         -> listCalendarEntries
        ("list":_) -> listCalendarEntries
-       ("add":_)  -> addCalendarEntry
+       ("add":info)  -> addCalendarEntry info
        _          -> printUsage
